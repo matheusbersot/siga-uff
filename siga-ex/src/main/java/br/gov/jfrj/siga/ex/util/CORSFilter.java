6532df1002083341d62b23d86904ee80be07f57d
Matheus Bersot <matheusbersot@id.uff.br>
2015-04-27 18:14:24 -0300

    REFS #32 por Matheus Bersot: Integrar applet assinador com javascript presente na página onde o applet é chamado

---
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/CORSFilter.java
@@ -0,0 +1,41 @@
+package br.gov.jfrj.siga.ex.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+	public CORSFilter() { }
+
+	public void init(FilterConfig fConfig) throws ServletException { }
+
+	public void destroy() {	}
+
+	public void doFilter(
+		ServletRequest request, ServletResponse response, 
+		FilterChain chain) throws IOException, ServletException {
+		
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Allow-Origin", "*"
+			);
+		((HttpServletResponse)response).addHeader(
+			"Access-Control-Allow-Credentials", "true"
+		);
+		
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Allow-Methods", "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL"
+			);
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Allow-Headers", "Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If"
+			);
+		
+		chain.doFilter(request, response);
+	}
+}
